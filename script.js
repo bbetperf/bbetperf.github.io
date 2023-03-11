c97928bae9f5277d42f382a45dcca1b00e787e9c
bbetperf <bbetperf@gmail.com>
2023-03-10 23:32:14 -0800

    upd

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+let i = 0;
+
 function ToggleMenu() {
     const button = document.getElementById('burger');
     const menu = document.getElementById('menu');
+    const button_menu = document.getElementById('button_menu');
 
     button.classList.toggle('active');
     menu.classList.toggle('active');
+    
+    if (i % 2 == 0) {
+        button_menu.textContent = 'close';
+    }
+    else button_menu.textContent = 'menu';
+
+    i++;
 }
 
 var offsetHeight = document.getElementsByClassName('fullheader')[0].offsetHeight;
-console.log(offsetHeight);
+console.log('Высота хедера:',offsetHeight);
 
 window.addEventListener('resize', setTop);
 window.addEventListener('scroll', setTop);
