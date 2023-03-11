e81e7ae2d9504a5f518568761493504fadf02847
bbetperf <bbetperf@gmail.com>
2023-03-10 20:42:46 -0800

    newupd

--- scroll.js
+++ scroll.js
@@ -2,13 +2,14 @@ let fullHeight = document.body.scrollHeight;
 console.log(fullHeight);
 let innerHeight = window.innerHeight;
 console.log(innerHeight);
+
 const progressBar = document.querySelector('.pagination_status_line>.line');
 
 window.addEventListener('scroll', fillProgressLine);
 window.addEventListener('resize', fillProgressLine);
 
 function fillProgressLine() {
-    fullHeight = document.body. scrollHeight;
-    innerHeight = window. innerHeight;
+    fullHeight = document.body.scrollHeight;
+    innerHeight = window.innerHeight;
     progressBar.style.height = (pageYOffset * 100 / (fullHeight - innerHeight)) + '%';
 }
