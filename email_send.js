cfebc815e7ab18414938ac8577c73062d35de272
Maksim Anisimov <71890037+bbetperf@users.noreply.github.com>
2024-04-09 20:38:00 -0700

    Create email_send.js

---
+++ email_send.js
@@ -0,0 +1,4 @@
+document.getElementById('email_button').addEventListener('click', function() {
+    var mailtoLink = 'mailto:bbetperf@gmail.com?subject=Hey, Max!&body=I am writing to you about the collaboration.';
+    window.location.href = mailtoLink;
+});
