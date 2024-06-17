afd31869dd21f1bf8dd17fa3625af995c47c80df
Maksim Anisimov <bbetperf@gmail.com>
2024-06-17 11:40:20 -0700

    upd email_send.js

--- email_send.js
+++ email_send.js
@@ -1,4 +1,4 @@
 document.getElementById('email_button').addEventListener('click', function() {
-    var mailtoLink = 'mailto:hello@bbetperf.com?subject=New friend.&body=Hey Max,';
+    var mailtoLink = 'mailto:hello@bbetperf.com?subject=Hello from Earth.&body=Hey Max,';
     window.location.href = mailtoLink;
 });
